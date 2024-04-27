9288cd30f41b3e8c54647d1ea16c7b34664e5a40
Ms-Juli <visuallady@mail.ru>
2024-04-27 19:18:19 +0500

    timer

--- practice/005-timer/script.js
+++ practice/005-timer/script.js
@@ -4,15 +4,30 @@ const minutesDOM = document.getElementById("minutes");
 const secondsDOM = document.getElementById("seconds");
 
 const currentDate = new Date();
-// const birthDay = new Date("2025-02-07");
-const birthDay = new Date("2024-04-28");
+const birthDay = new Date("2025-02-07");
 let diff = Math.floor((birthDay - currentDate)/(1000));
 
-console.log(Math.floor (diff));
+console.log('Result diff:', diff);
 
 setInterval(()=> {
     diff = diff - 1;
 
-    let day = diff/60/60/24;
-    console.log(day);
+    let diffDay = diff % (60*60*24);
+    let day = (diff - diffDay) / (60*60*24);
+
+    daysDOM.innerText = day;
+
+    let diffHours = diffDay % (60*60);
+    let hours = (diffDay - diffHours) / (60*60);
+
+    hoursDOM.innerText = hours;
+
+    let diffMinutes = diffHours % (60);
+    let minutes = (diffHours - diffMinutes) / (60);
+
+    minutesDOM.innerText = minutes;
+    secondsDOM.innerText = diffMinutes;
+
+    let outputTime =` Days: ${day}, Hours: ${hours}, Minutes: ${minutes}, Seconds: ${diffMinutes}`;
+    console.log(outputTime);
 },1000);
