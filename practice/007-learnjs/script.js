9021705897b82779dcc31c01182e07215e3a40a7
Ms-Juli <visuallady@mail.ru>
2024-05-23 14:10:39 +0500

    007-learnjs

---
+++ practice/007-learnjs/script.js
@@ -0,0 +1,26 @@
+"use strict"; // этот код работает в современном режиме
+alert ("Я JavaScript!");
+
+// alert ("Hello");
+// [1, 2].forEach(alert);
+
+/*let message;
+message = "Hello";
+alert (message);*/
+
+/*let admin;
+let name;
+name = "Джон";
+admin = name; //Копирование значения из одной переменной в другую
+alert (admin);
+
+alert (`Привет, ${name}`); //Вставили выражение в строку*/
+
+/*let age = prompt('Сколько тебе лет?');
+alert(`Тебе ${age} лет!`);
+
+let isBoss = confirm("Ты сздесь главный?");
+alert( isBoss );*/
+
+let name = prompt("Как тебя зовут?");
+alert(`Тебя зовут ${name}!`);
