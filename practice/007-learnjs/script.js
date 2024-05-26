9eec566ca0201d98f0d8dcc1f4bf0991f83d118c
Ms-Juli <visuallady@mail.ru>
2024-05-26 21:11:24 +0500

    007-learnjs

--- practice/007-learnjs/script.js
+++ practice/007-learnjs/script.js
@@ -22,5 +22,24 @@ alert(`Тебе ${age} лет!`);
 let isBoss = confirm("Ты сздесь главный?");
 alert( isBoss );*/
 
-let name = prompt("Как тебя зовут?");
-alert(`Тебя зовут ${name}!`);
+/*let name = prompt("Как тебя зовут?");
+alert(`Тебя зовут ${name}!`);*/
+
+let value = true;
+alert(typeof value); //boolean
+
+value = String(value); //теперь value это строка true
+alert(typeof value); //string
+
+let age = Number("Любая строка");
+alert(age); //Nan, преобразование не удалось
+
+let x = 1;
+x = -x;
+alert(x); // -1
+
+let n = 2;
+n += 5;
+n *= 2; 
+alert(n);
+
