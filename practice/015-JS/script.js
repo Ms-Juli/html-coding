4293d87458077b4d51bb09c69cf56d07811b2ec8
Ms-Juli <visuallady@mail.ru>
2025-03-28 15:38:03 +0500

    function

--- practice/015-JS/script.js
+++ practice/015-JS/script.js
@@ -1,32 +1,106 @@
 "use strict";
-alert("Привет, мир!");
-alert("Я JavaScript!");
-/*alert(3 +
-    1
-    + 2);*/
-//Объявляем переменную и пределяем переменную
-let message;
-message = "Hello!"; //Сохранить строку "Hello!" в переменной с именем message
-alert(message); //Показывает содержимое переменной
 
-message = "Hello!";
-alert(message);
+// let name = "Джон";
+// let admin = name;
+// alert(admin);
 
-message = "Hello!";
-message = "World"; //значение переменной изменено
-alert(message);
+//Переменная для названия планеты земля
+// let currentSiteUser = "John";
+// let outPlanetName = "Earth";
 
-const myBirthday = "18.02.2000"; //константная переменная. Неизменяемая переменная
+// let nameUser = prompt ("Как тебя зовут?");
+// alert(nameUser);
 
-let admin, name;
-name = "Джон";
-admin = name;
-alert(admin);
+// let a = prompt("Первое число?", 1);
+// let b = prompt("Второе число?", 2);
 
-let str = "Привет";
-let str2 = 'Одинарные кавычки тоже подойдут';
-let phrase = `Обратные кавычки позволяют встраивать переменные ${str}`;
+// alert(+a + +b); // 12
 
-name = "Лена";
-alert(`Привет, ${name}!`);
+// 
+
+// let name = prompt ("Какое «официальное» название JavaScript?", "");
+// if (name == "ECMAScript") {
+//     alert ("Верно!");
+// } else {
+//     alert ("Не знаете? ECMAScript!");
+// }
+
+// let number = prompt("Введите число: ", "");
+// if (number > 0) {
+//     alert(1);
+// } else if (number < 0) {
+//     alert (-1);
+// } else {
+//     alert(0);
+// }
+
+// let result;
+// result = (1 + 3 < 4) ? "Мало" : "Много";
+
+// let login;
+// let message = (login == 'Сотрудник') ? 'Привет' :
+//   (login == 'Директор') ? 'Здравствуйте' :
+//   (login == '') ? 'Нет логина' :
+//   '';
+
+// let age = prompt("Введите число: ");
+// if (age < 14) {
+//     alert("Мало");
+// } else if (age > 90) {
+//     alert("Too much");
+// } else {
+//     alert("right!");
+// }
+
+// if (age >= 14 && age <= 90)
+
+// let userName = prompt("Кто там?", '');
+
+// if (userName === 'Админ') {
+
+//   let pass = prompt('Пароль?', '');
+
+//   if (pass === 'Я главный') {
+//     alert( 'Здравствуйте!' );
+//   } else if (pass === '' || pass === null) {
+//     alert( 'Отменено' );
+//   } else {
+//     alert( 'Неверный пароль' );
+//   }
+
+// } else if (userName === '' || userName === null) {
+//   alert( 'Отменено' );
+// } else {
+//   alert( "Я вас не знаю" );
+// }
+
+//Для проверки на чётность мы здесь используем оператор получения остатка от деления %.
+// for (let i = 2; i <= 10; i++) {
+//   if (i % 2 == 0) {
+//     alert( i );
+//   }
+// }
+
+// Переписан цикл с for на while
+// for (let i = 0; i < 3; i++) {
+//   alert( `number ${i}!` );
+// }
+
+// let i = 0;
+// while (i < 3) {
+//   alert (`number ${i} !`);
+//   i++;
+// }
+
+// 
+function checkAge(age) {
+  return(age > 18 ? true : confirm('Родители разрешили?'));
+}
+
+function checkAge(age) {
+  return (age > 18) || confirm('Родители разрешили?');
+}
+
+
+
 
